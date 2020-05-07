b2cd6e04c3ac6af2fb64e1309311b54f3cb1e20c
Gordon <gordon.boeer@web.de>
2020-05-07 13:25:03 +0200

    refactoring

--- cogs.py
+++ cogs.py
@@ -42,26 +42,30 @@ class PokerChipsCog(commands.Cog, name="Poker Chips"):
             await ctx.send('{} spend {} chips, {} left.'.format(ctx.author, amount, self.chips[ctx.author]))
 
 
-
-
-
 class GameManagementCog(commands.Cog, name="Management"):
 
     def __init__(self, bot):
         self.bot = bot
         self.chips = PokerChipsCog
-        self.players_list = []
+        self.character_list = []
         self.npc_list = []
+
+    @commands.command(name='restart', help='Check and load a list of previously created characters.')
+    #@commands.has_role('gamemaster')
+    async def restart(self, ctx):
+        await ctx.send('Looking for existing characters...')
+        self.load_session()
+        await ctx.send('Found {} characters and {} NPCs'.format(len(self.character_list), len(self.npc_list)))
 
     def save_session(self):
         with open("./session/players.json", "w") as output_file:
-            json.dump([p.__dict__ for p in self.players_list], output_file)
+            json.dump([p.__dict__ for p in self.character_list], output_file)
         with open("./session/npcs.json", "w") as output_file:
             json.dump([p.__dict__ for p in self.npc_list], output_file)
 
     # load a list of previously stored characters
     def load_session(self):
-        self.players_list.clear()
+        self.character_list.clear()
         self.npc_list.clear()
         if Path('./session/players.json').exists():
             with open("./session/players.json", "r") as read_file:
@@ -69,7 +73,7 @@ class GameManagementCog(commands.Cog, name="Management"):
                 for d in data:
                     p = PlayerCharacter()
                     p.__dict__.update(d)
-                    self.players_list.append(p)
+                    self.character_list.append(p)
         if Path('./session/npcs.json').exists():
             with open("./session/npcs.json", "r") as read_file:
                 data = json.load(read_file)
@@ -81,7 +85,7 @@ class GameManagementCog(commands.Cog, name="Management"):
     @commands.command(name='new_pc', help='Create a new player character.')
     async def add_new_player_character(self, ctx, character_name: str):
         new_player = PlayerCharacter(character_name=character_name, user=ctx.author)
-        self.players_list.append(new_player)
+        self.character_list.append(new_player)
 
         self.save_session()
         await ctx.send('Added {} for {}'.format(character_name, ctx.author))
@@ -90,7 +94,7 @@ class GameManagementCog(commands.Cog, name="Management"):
     #@commands.has_role('gamemaster')
     async def assign_player_character(self, ctx, character_name: str, player_name: str):
         found = False
-        for p in self.players_list:
+        for p in self.character_list:
             if character_name == p.character_name:
                 found = True
                 p.user = player_name
@@ -101,26 +105,18 @@ class GameManagementCog(commands.Cog, name="Management"):
         else:
             await ctx.send('Couldn\'t find character {}'.format(character_name))
 
-
-    @commands.command(name='restart', help='Check and load a list of previously created characters.')
-    #@commands.has_role('gamemaster')
-    async def restart(self, ctx):
-        await ctx.send('Looking for existing characters...')
-        self.load_session()
-        await ctx.send('Found {} characters and {} NPCs'.format(len(self.players_list), len(self.npc_list)))
-
     @commands.command(name='list_characters', aliases=["lc", "players"], help='Print the list of available characters.')
     async def list_characters(self, ctx):
 
-        if len(self.players_list) < 1:
+        if len(self.character_list) < 1:
             await ctx.send('No characters loaded.')
         else:
             embed = Embed(title="Available player characters", color=0x00ff00)
-            for p in self.players_list:
+            for p in self.character_list:
                 embed.add_field(name=p.character_name, value="{} \n {}".format(p.user, p.friend), inline=False)
             await ctx.send(embed=embed)
 
-    @commands.command(name='list_npcs', help='Print the list of NPCs.')
+    @commands.command(name='list_npcs', aliases=["lnpc"], help='Print the list of NPCs.')
     async def list_npcs(self, ctx):
         if len(self.npc_list) < 1:
             await ctx.send('No NPCs loaded.')
